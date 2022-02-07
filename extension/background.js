72c433e963ec2064b52accc3e6dd3204ec54ea8a
Anmol Shrestha <shrestha.anmol.as@gmail.com>
2022-02-06 20:26:41 -0500

    checking refactor and working

--- extension/background.js
+++ extension/background.js
@@ -1,13 +1,11 @@
-chrome.runtime.onInstalled.addListener(() => {
-  const enableOrDisableExt = (tab) => {
-    const isWordle = tab.url.startsWith(
-      'https://www.powerlanguage.co.uk/wordle/'
-    );
-    isWordle
-      ? chrome.action.enable(tab.tabId)
-      : chrome.action.disable(tab.tabId);
-  };
+const enableOrDisableExt = (tab) => {
+  const isWordle = tab.url.startsWith(
+    'https://www.powerlanguage.co.uk/wordle/'
+  );
+  isWordle ? chrome.action.enable(tab.tabId) : chrome.action.disable(tab.tabId);
+};
 
+chrome.runtime.onInstalled.addListener(() => {
   chrome.tabs.onActivated.addListener(async (info) => {
     const tab = await chrome.tabs.get(info.tabId);
     enableOrDisableExt(tab);
@@ -15,7 +13,7 @@ chrome.runtime.onInstalled.addListener(() => {
 
   chrome.tabs.onUpdated.addListener(async (tabId, changeInfo) => {
     const tab = await chrome.tabs.get(tabId);
-    if (tab.active && changeInfo.url) {
+    if (tab.active) {
       enableOrDisableExt(tab);
     }
   });
