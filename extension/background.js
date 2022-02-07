9ac2b810e9d43f5cce73b29d9ebfe5ee3673252d
Anmol Shrestha <shrestha.anmol.as@gmail.com>
2022-02-06 20:02:18 -0500

    make it only work for wordle website

--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,22 @@
 chrome.runtime.onInstalled.addListener(() => {
-  console.log(`we here`);
+  const enableOrDisableExt = (tab) => {
+    const isWordle = tab.url.startsWith(
+      'https://www.powerlanguage.co.uk/wordle/'
+    );
+    isWordle
+      ? chrome.action.enable(tab.tabId)
+      : chrome.action.disable(tab.tabId);
+  };
+
+  chrome.tabs.onActivated.addListener(async (info) => {
+    const tab = await chrome.tabs.get(info.tabId);
+    enableOrDisableExt(tab);
+  });
+
+  chrome.tabs.onUpdated.addListener(async (tabId, changeInfo) => {
+    const tab = await chrome.tabs.get(tabId);
+    if (tab.active && changeInfo.url) {
+      enableOrDisableExt(tab);
+    }
+  });
 });
